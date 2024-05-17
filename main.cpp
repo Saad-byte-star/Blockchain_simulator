f1ce8190b41721fd19618cb34931003c39ba8576
Saad-byte-star <143700947+Saad-byte-star@users.noreply.github.com>
2024-05-18 02:25:41 +0500

    Update main.cpp
    Implemented Functionality for Block Class Member Module : getCurrentTimestamp. 
    The Module returns a Date Time String of the Format YYYY-MM-DD HH:MM:SS
    
    Comments have been Added at each line for explanation of what each line does.
    
    and Lastly Some Questions have been added regarding the module , Read them and change the function Accordingly.
    also Answer them for me

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 # include <iostream>
 # include "sqlite3.h"
 # include <vector>
+#include <ctime> // Library to Use DataTime Capabilities in CPP
 using namespace std;
 
 class Transaction;
@@ -71,7 +72,19 @@ class Block
              /* TODO
              to implement this function go through the experimantation.cpp file
               */ 
-        return ""; 
+            time_t currentTimestamp = time(nullptr); // THIS LINE GETS THE CURRENT TIME IN SECOND SINCE THE ENOCH ( WHICH IS A STANDARD STARTING POINT BY UNIX FOR RECORDING TIME ) 
+            tm* localTime = localtime(&currentTimestamp); // THE TM IS A CONSTRUCTOR IN THE CTIME LIBRARY , USED TO TIME IN SECONDS INTO LOCAL TIME FORMAT
+            string dateTime = to_string(1900 + localTime->tm_year) + "-" +
+                            to_string(1 + localTime->tm_mon) + "-" +
+                            to_string(localTime->tm_mday) + " " +
+                            to_string(localTime->tm_hour) + ":" +
+                            to_string(localTime->tm_min) + ":" +
+                            to_string(localTime->tm_sec); // ALL THIS WILL RETURN A STRING OF DATE AND TIME WHICH IF NEEDED WE CAN PARSE AND USE LATER
+            
+        return dateTime; 
+            // QEUSTIONS 
+            // IS THIS METHOD FOR SETTING THE TIMESTAMP FOR THE BLOCK CREATION AND UPDATION? I MEAN WHENVEVER THE BLOCK IS UPDATED OR IF ITS CREATED , THIS METHOD WILL BE CALLED AND WILL UPDATE THE TIMESTAMP? RIGHT?
+            // IF THE ABOVE IS RIGHT THEN WILL WE STORE THE UPDATION TIMESTAMP IN THE DATABASE FOR TRACKING FOR WILL BE UPDATE THE TIMESTAMP PROPERTY VARIABLE AND SET IT TO THE LATEST TIMESTAMP ( WHICH IS THE UPDATION TIMESTAMP )
         }
 
         // Method to calculate block hash (you can implement your own)
