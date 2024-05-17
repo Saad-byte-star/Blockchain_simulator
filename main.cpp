27ea9f5d6bd903cd03ac3c52e3c8976634cbae8e
Abu Bakkar Siddique <122197397+Abu-bakkar-siddique@users.noreply.github.com>
2024-05-17 23:43:41 +0500

    worked on block creation

--- main.cpp
+++ main.cpp
@@ -1,24 +1,93 @@
 # include <iostream>
 # include "sqlite3.h"
+# include <vector>
 using namespace std;
 
+class Transaction;
 class Block
 {
     private:
-        string previous_block_hash;
-        /* timestamp will be assigned a value at the time of block object creation
-        more details in experimentation file*/
+        string blockHash;
+        string previousBlockHash;
         string timestamp;
-        // A unified hash that is a combination of all the transactions hashes
-        string Merkle_Root;
-        //A random number used in mining (idk)
-        int Nonce;
-        //deficulty level of the block in mining
-        int difficulty_level;
-        //nuLL by default
-        string next_block_hash;
-};
+        string merkleRoot;
+        int nonce;
+        int difficultyLevel;
+        vector<Transaction> transactions;
+        
+    public:
+        Block(const string& prevHash, int diffLevel) : previousBlockHash(prevHash), difficultyLevel(diffLevel) {
+            timestamp = getCurrentTimestamp(); // Initialize timestamp
+            nonce = 0; // Initialize nonce
+        }
 
+        // Getter methods
+        string getBlockHash()  
+        {
+             return blockHash;
+        }
+        string getPreviousBlockHash() 
+        { 
+            return previousBlockHash; 
+        }
+        string getTimestamp() 
+        { 
+            return timestamp; 
+        }
+        string getMerkleRoot()
+        { 
+            return merkleRoot; 
+        }
+        int getNonce() 
+        {
+            return nonce;
+        }
+        int getDifficultyLevel() 
+        { 
+            return difficultyLevel;
+        }
+        vector<Transaction> getTransactions()  
+        { 
+            return transactions; 
+        }
+
+        // Setter methods
+        void setBlockHash(string hash) 
+        { 
+            blockHash = hash; 
+        }
+        void setMerkleRoot( string root) 
+        { 
+            merkleRoot = root;
+        }
+        void addTransaction( Transaction& txn) 
+        { 
+            transactions.push_back(txn);
+        }
+
+        // Utility method to get current timestamp (you can implement your own)
+        string getCurrentTimestamp() 
+        {
+             /* TODO
+             to implement this function go through the experimantation.cpp file
+              */ 
+        return ""; 
+        }
+
+        // Method to calculate block hash (you can implement your own)
+        string calculateBlockHash() 
+        { /* TODO
+            read about block hashing and hashing algorithm to complete this
+         */ return ""; 
+        }
+
+        // Method to mine the block (you can implement your own)
+        void mineBlock() 
+        { /* TODO
+         read about mining */ 
+        }
+};
+        
 int main()
 {
     return 0;
